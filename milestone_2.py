91370845639b1acd93d20fc51e8cbc67b0fa64bd
Stephen Delves <stephen.delves@hotmail.co.uk>
2023-04-09 21:19:09 +0100

    input guess and check for alphabetical input

--- milestone_2.py
+++ milestone_2.py
@@ -1,2 +1,15 @@
+import random
+
 words_list = ["banana", "kiwi", "blueberry", "tangerine", "mango"]
 print(words_list)
+
+word = random.choice(words_list)
+print(word)
+
+guess = input("Make a guess: ")
+print(guess)
+
+if (guess.isalpha() == True) and (len(guess) == 1):
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
